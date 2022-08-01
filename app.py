7f5e0b780d3116fcc16ed89590c30d557244be69
kyongsong <2534736239@qq.com>
2022-08-01 17:27:18 +0800

    add the animation of the trajectory

--- app.py
+++ app.py
@@ -74,39 +74,7 @@ def movement():
           
 
 
-@app.route("/Next_Frame", methods=['POST'])
-def NextFrame():
-     try:
-         # 这里发来的请求需要加个参数 event_id = xxx，选择播放第几个回合的轨迹
-         global event_id_value
-         global index_value
-        
-         
-         index_value+=1
 
-         #需要判断下一帧是不是最后一帧，如果是最后一帧需要进行切换下一个回合
-         
-
-         possession_data_path = os.path.join('0021500001', str(event_id_value))
-
-         with open(os.path.join(possession_data_path, "metadata.json"), "r") as f:
-             metadata = json.load(f)
-
-         with open(os.path.join(possession_data_path, "movement_refined_shot_clock.json"), "r") as f:
-             movement_data = json.load(f)
-
-         offensive_team = metadata[metadata['offensive_team']]['teamid']
-         start_index = metadata['possession_start_index'] + index_value
-         end_index = metadata['possession_end_index']
-         movement = []
-         for item in movement_data[start_index: end_index + 1]:
-             movement.append(item)
-
-         return {'message': 'success!', 'movement': movement, 'metadata': metadata}
-
-     except Exception as _:
-         print(_)
-         return json.dumps({'message': 'failed'})
 
 if __name__ == "__main__":
      app.run(threaded=True, host="127.0.0.1", port=8081, debug=True)
